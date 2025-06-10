0aab8369728836f9526c7dfdebbd3eb7ba43f988
spamegg <spamegg@pm.me>
2025-06-10 22:53:02 +0300

    stream transform, exercises

--- src/Exercises.scala
+++ src/Exercises.scala
@@ -1,8 +1,7 @@
 package fs2explore
 package exercises
 
-import fs2.Stream
-import fs2.Chunk
+import fs2.*
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 
@@ -19,13 +18,11 @@ def repeat[F[_], O](s: Stream[F, O]): Stream[F, O] = s ++ repeat(s)
 
 // Stream(1,2,3).drain.toList
 // res28: List[Nothing] = List()
-def drain[F[_], O](s: Stream[F, O]): Stream[F, Nothing] = s
-  .flatMap(o => Stream.empty[F])
+def drain[F[_], O](s: Stream[F, O]): Stream[F, Nothing] = s.flatMap(o => Stream.empty[F])
 
 // Stream.exec(IO.println("!!")).compile.toVector.unsafeRunSync()
 // res29: Vector[Nothing] = Vector()
-def exec[F[_]](action: F[Unit]): Stream[F, Nothing] =
-  drain(Stream.eval(action))
+def exec[F[_]](action: F[Unit]): Stream[F, Nothing] = drain(Stream.eval(action))
 
 // (Stream(1,2) ++ Stream(3).map(_ => throw new Exception("nooo!!!"))).attempt.toList
 // res30: List[Either[Throwable, Int]] = List(
@@ -38,6 +35,29 @@ def attempt[F[_], O](s: Stream[F, O]): Stream[F, Either[Throwable, O]] = s
   .handleErrorWith(err => Stream.emit(Left(err)))
 
 // Stream Transforming
+// Try implementing takeWhile, intersperse, and scan:
+// Stream.range(0,100).takeWhile(_ < 7).toList
+// res42: List[Int] = List(0, 1, 2, 3, 4, 5, 6)
+def takeWhile[F[_], O](s: Stream[F, O])(p: O => Boolean): Stream[F, O] =
+  def go(st: Stream[F, O]): Pull[F, O, Unit] = st.pull.uncons1
+    .flatMap:
+      case Some(o, stream) => if p(o) then Pull.output1(o) >> go(stream) else Pull.done
+      case None            => Pull.done
+  go(s).stream
+
+// Stream("Alice","Bob","Carol").intersperse("|").toList
+// res43: List[String] = List("Alice", "|", "Bob", "|", "Carol")
+def intersperse[F[_], O, O2 >: O](s: Stream[F, O])(separator: O2): Stream[F, O2] =
+  def go(st: Stream[F, O]): Pull[F, O2, Unit] = st.pull.uncons1
+    .flatMap:
+      case Some(o, stream) =>
+        Pull.output1(separator) >> Pull.output1(o) >> go(stream)
+      case None => Pull.done
+  go(s).stream.tail
+
+// Stream.range(1,10).scan(0)(_ + _).toList // running sum
+// res44: List[Int] = List(0, 1, 3, 6, 10, 15, 21, 28, 36, 45)
+def scan[F[_], O, O2](s: Stream[F, O])(z: O2)(f: (O2, O) => O2): Stream[F, O2] = ???
 
 @main
 def run: Unit =
@@ -46,4 +66,11 @@ def run: Unit =
     attempt(Stream(1, 2) ++ Stream(3).map(_ => throw new Exception("nooo!!!"))).toList
   println(drain(Stream(1, 2, 3)).toList)
   println:
-    exec(IO.println("!!")).compile.toVector.unsafeRunSync()
+    exec(IO.println("!!")).compile.toVector
+      .unsafeRunSync()
+
+  // Stream transforming
+  println(takeWhile(Stream.range(0, 10))(x => x % 2 == 0 || x % 3 == 1).toList)
+  println(takeWhile(Stream.range(0, 100))(_ < 7).toList)
+  println(intersperse(Stream("Alice", "Bob", "Carol"))("|").toList)
+  // println(scan(Stream.range(1, 10))(0)(_ + _).toList)
--- src/StreamTransform.scala
+++ src/StreamTransform.scala
@@ -14,6 +14,29 @@ def tk[F[_], O](n: Long): Pipe[F, O, O] = in =>
           case m if m < k => (k - m, c)
           case _          => (0, c.take(k.toInt))
 
+// transforming using Pull[F[_], O, R]
+// F effect, O output, R result, forms a monad in R
+val p1 = Pull.output1(1) // Pull[Nothing, Int, Unit]
+val s1 = p1.stream       // Stream[Nothing, Int]
+val p2 = s1.pull.echo    // same as p1
+val p3 = s1.pull.uncons  // Pull[Nothing,Nothing,Option[(Chunk[Int],Stream[Nothing,Int])]]
+
+def tk2[F[_], O](n: Long): Pipe[F, O, O] =
+  def go(s: Stream[F, O], k: Long): Pull[F, O, Unit] = s.pull.uncons
+    .flatMap:
+      case Some((hd, tl)) =>
+        hd.size match
+          case m if m <= k => Pull.output(hd) >> go(tl, k - m)
+          case _           => Pull.output(hd.take(k.toInt))
+      case None => Pull.done
+  in => go(in, n).stream
+
+def tk3[F[_], O](n: Long): Pipe[F, O, O] =
+  in => in.pull.take(n).void.stream
+
 @main
 def run: Unit =
-  println(Stream(1, 2, 3, 4).through(tk(3)).toList)
+  val s = Stream(1, 2, 3, 4)
+  println(s.through(tk(3)).toList)
+  println(s.through(tk2(3)).toList)
+  println(s.through(tk3(3)).toList)
